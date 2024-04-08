22be2dc3c211f21214191ebd466a307a0de46ba1
theoneLYJ <theone.lyj@gmail.com>
2024-04-08 08:39:40 +0800

    fix debugger.lua

--- lua/plugins/configs/debugger.lua
+++ lua/plugins/configs/debugger.lua
@@ -1,7 +1,7 @@
 return {
 	{
 		"mfussenegger/nvim-dap",
-		dependencies = { "rcarriga/nvim-dap-ui" },
+		dependencies = { "rcarriga/nvim-dap-ui", "nvim-neotest/nvim-nio" },
 		config = function()
 			local dap, dapui = require("dap"), require("dapui")
 			vim.fn.sign_define('DapBreakpoint', {text = mo.styles.icons.dap.signs.breakpoint, texthl = '', linehl = '', numhl = ''})
